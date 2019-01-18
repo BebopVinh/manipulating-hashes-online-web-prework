7df58c51408fe279ba4e03e87db9bf5b13d56595
Vinh Do <vinhdo07@gmail.com>
2019-01-18 22:28:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,12 @@ def first_challenge
   }
 
   #your code here
-  contacts.each do |contact, value|
-    value
+  contacts.each do |contact, data|
+    if data == "Freddy Mercury"
+      if value == :favorite_icecream_flavors
+        :favorite_icecream_flavors.delete_if("strawberry")
+      end
+    end
   end
   #remember to return your newly altered contacts hash!
   contacts
